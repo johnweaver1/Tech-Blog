c32489b12555f58f6695298a3875c2f809ef0d0b
John W <john.weaver94@utexas.edu>
2023-02-20 11:05:16 -0600

    changed models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,22 @@
 const User = require('./User');
-const Project = require('./Project');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
+User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Comment };

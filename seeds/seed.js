277fef8aeef53b1c6c1cf824794e746bab86bf5d
John W <john.weaver94@utexas.edu>
2023-02-20 12:38:48 -0600

    changed the seed model to put users, commnets, posts in the seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,81 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post, Comment } = require('../models');
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const users = [
+  {
+      name: "John",
+      password: "john1"
+  },
+  {
+      name: "chase",
+      password: "chase1"
+  },
+  {    name: "weaver",
+      password: "weaver1"
+  },
+
+]
+
+const posts = [
+  {
+      title: "I shouldn't ahve skipped class",
+      description: "Eh, but I'm fine",
+      date_created: "1/22/2022",
+      user_id: 1
+  },
+  {
+      title: "This class definitely won't stress me out",
+      description: "it did",
+      date_created: "11/10/2022",
+      user_id: 2
+  },
+  {
+      title: "Help I'm stuck in a computer",
+      description: "Can anyone read this?",
+      date_created: "01/20/2023",
+      user_id: 2
+  },
+  {
+      title: "Just out here blogging about class",
+      description: "Css is crazy riiiiiiiiiight?",
+      date_created: "2/14/2023",
+      user_id: 3
+  },
+]
+
+const comments = [
+  {
+      comment_body: "Keep working it'll get better!",
+      post_id: 1,
+  },
+  {
+      comment_body: "it will!",
+      post_id: 2,
+  },
+  {
+      comment_body: "Top men are on it!",
+      post_id: 3,
+  },
+  {
+      comment_body: "Have you ever looked at a css file? It's crazy!",
+      post_id: 4,
+
+  },
+
+]
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
-
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
-
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+  try{
+    await sequelize.sync({force:true})
+    await User.bulkCreate(users,{
+        individualHooks:true
     });
-  }
-
-  process.exit(0);
-};
+    await Post.bulkCreate(posts);
+    await Comment.bulkCreate(comments);
+    process.exit(0);
+} catch(err){
+    console.log(err)
+}
+}
 
 seedDatabase();
